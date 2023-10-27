d9584576afd0f8bfb033cd2aed643c0f43f672e9
milind-nair <nairmilind3@gmail.com>
2023-10-28 00:57:23 +0530

    re-placed component

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -11,7 +11,15 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { setConstants } from "../../utils/utils";
 
-const InputBox = ({ parameter }: { parameter: string }) => {
+const InputBox = ({
+  parameter,
+  value,
+  setValue,
+}: {
+  parameter: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}) => {
   const NUM_ITEMS = setConstants(parameter);
   const [everyN, setEveryN] = React.useState("");
   const [checkboxes, setCheckboxes] = React.useState(
@@ -28,21 +36,28 @@ const InputBox = ({ parameter }: { parameter: string }) => {
     const newCheckboxes = [...checkboxes];
     newCheckboxes[index] = !newCheckboxes[index];
     setCheckboxes(newCheckboxes);
+    const filteredList = newCheckboxes
+      .map((element, index) => (element ? index : -1))
+      .filter((index) => index !== -1);
+    const newVal = filteredList.join(",");
+    setValue(newVal);
   };
   return (
-    <Box sx={{ minWidth: 120, mx: 10, mt: "2%" }}>
+    <Box sx={{ minWidth: 100, mx: 10, mt: "2%" }}>
       <FormControl fullWidth>
-        <Grid container spacing={1.5} sx={{ justifyContent: "center" }}>
+        <Grid container spacing={2} sx={{ justifyContent: "space-around" }}>
           <Grid item>
             <Button
               variant="contained"
               onClick={() => {
                 setSelection("every");
+                setValue("*");
               }}
             >
               Every {parameter}
             </Button>
           </Grid>
+
           <Grid item>
             <Button
               variant="contained"
@@ -66,8 +81,55 @@ const InputBox = ({ parameter }: { parameter: string }) => {
               </Select>{" "}
               {parameter}s
             </Button>
-            <InputLabel id="demo-simple-select-label"></InputLabel>
           </Grid>
+          {selection === "everyN" && (
+            <>
+              <Grid item>
+                <TextField
+                  label="From"
+                  id="outlined-basic"
+                  size="small"
+                  select
+                  fullWidth
+                  sx={{ minWidth: "10ch", color: "black", mx: -15 }}
+                  value={from}
+                  onChange={(event) => {
+                    setFrom(event.target.value as string);
+                  }}
+                  variant="outlined"
+                >
+                  {menuItems.map((value, index) => (
+                    <MenuItem key={index} value={value}>
+                      {value}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="outlined-basic"
+                  label="To"
+                  select
+                  size="small"
+                  variant="outlined"
+                  fullWidth
+                  value={to}
+                  sx={{ minWidth: "10ch", ml: -20 }}
+                  onChange={(event) => {
+                    setTo(event.target.value as string);
+                  }}
+                >
+                  {menuItems.map((value, index) => (
+                    <MenuItem key={index} value={value}>
+                      {value}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+            </>
+          )}
+
+          <InputLabel id="demo-simple-select-label"></InputLabel>
           <Grid item>
             <Button
               variant="contained"
@@ -83,53 +145,10 @@ const InputBox = ({ parameter }: { parameter: string }) => {
           <Grid
             container
             spacing={3}
-            sx={{ justifyContent: "center", mt: "1%" }}
-          >
-            <Grid item>
-              <TextField
-                label="From"
-                id="outlined-basic"
-                size="small"
-                select
-                fullWidth
-                sx={{ m: 1, minWidth: "10ch", color: "black" }}
-                value={from}
-                onChange={(event) => {
-                  setFrom(event.target.value as string);
-                }}
-                variant="outlined"
-              >
-                {menuItems.map((value, index) => (
-                  <MenuItem key={index} value={value}>
-                    {value}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-            <Grid item>
-              <TextField
-                id="outlined-basic"
-                label="To"
-                select
-                size="small"
-                variant="outlined"
-                fullWidth
-                value={to}
-                sx={{ m: 1, minWidth: "10ch" }}
-                onChange={(event) => {
-                  setTo(event.target.value as string);
-                }}
-              >
-                {menuItems.map((value, index) => (
-                  <MenuItem key={index} value={value}>
-                    {value}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-          </Grid>
+            sx={{ justifyContent: "space-around", mt: "1%" }}
+          ></Grid>
         )}
-        <Grid container sx={{ justifyContent: "center", mt: "5%" }}>
+        <Grid container sx={{ justifyContent: "space-between", mt: "5%" }}>
           {selection === "particular" &&
             checkboxes.map((checked, index) => (
               <Grid item xs={2}>
