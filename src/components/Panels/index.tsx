9d8cfe5bbc7f10b85b45d8d4695e118c3298f1b3
milind-nair <nairmilind3@gmail.com>
2023-10-28 00:57:08 +0530

    added output preview

--- src/components/Panels/index.tsx
+++ src/components/Panels/index.tsx
@@ -9,6 +9,12 @@ import DoneButton from "./DoneButton";
 
 export default function Panels() {
   const [expanded, setExpanded] = React.useState<string | false>(false);
+  const [param1, setParam1] = React.useState("*");
+  const [param2, setParam2] = React.useState("*");
+  const [param3, setParam3] = React.useState("*");
+  const [param4, setParam4] = React.useState("*");
+  const [param5, setParam5] = React.useState("*");
+  const [param6, setParam6] = React.useState("*");
   const handleChange =
     (panel: string) => (_event: React.SyntheticEvent, isExpanded: boolean) => {
       setExpanded(isExpanded ? panel : false);
@@ -27,11 +33,11 @@ export default function Panels() {
           <Typography variant="h6" sx={{ width: "50%", flexShrink: 0 }}>
             Seconds
           </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            {/* I am an accordion */}
+          <Typography variant="h6" sx={{ color: "text.secondary" }}>
+            {param1}
           </Typography>
         </AccordionSummary>
-        <InputBox parameter="Second" />
+        <InputBox parameter="Second" value={param1} setValue={setParam1} />
         <DoneButton setExpanded={setExpanded} panel={"panel1"} />
       </Accordion>
       <Accordion
@@ -43,14 +49,14 @@ export default function Panels() {
           aria-controls="panel2bh-content"
           id="panel2bh-header"
         >
-          <Typography variant="h6" sx={{ width: "33%", flexShrink: 0 }}>
+          <Typography variant="h6" sx={{ width: "50%", flexShrink: 0 }}>
             Minutes
           </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            {/* You are currently not an owner */}
+          <Typography variant="h6" sx={{ color: "text.secondary" }}>
+            {param2}
           </Typography>
         </AccordionSummary>
-        <InputBox parameter="Minute" />
+        <InputBox parameter="Minute" value={param2} setValue={setParam2} />
         <DoneButton setExpanded={setExpanded} panel={"panel2"} />
         <AccordionDetails>
           <Typography>
@@ -69,14 +75,14 @@ export default function Panels() {
           aria-controls="panel3bh-content"
           id="panel3bh-header"
         >
-          <Typography variant="h6" sx={{ width: "33%", flexShrink: 0 }}>
+          <Typography variant="h6" sx={{ width: "50%", flexShrink: 0 }}>
             Hours
           </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            {/* Filtering has been entirely disabled for whole web server */}
+          <Typography variant="h6" sx={{ color: "text.secondary" }}>
+            {param3}
           </Typography>
         </AccordionSummary>
-        <InputBox parameter="Hour" />
+        <InputBox parameter="Hour" value={param3} setValue={setParam3} />
         <DoneButton setExpanded={setExpanded} panel={"panel3"} />
       </Accordion>
       <Accordion
@@ -88,18 +94,20 @@ export default function Panels() {
           aria-controls="panel5bh-content"
           id="panel5bh-header"
         >
-          <Typography variant="h6" sx={{ width: "33%", flexShrink: 0 }}>
+          <Typography variant="h6" sx={{ width: "50%", flexShrink: 0 }}>
             Day Of Month
           </Typography>
-        </AccordionSummary>
-        <InputBox parameter="Day-Of-Month" />
-        <DoneButton setExpanded={setExpanded} panel={"panel4"} />
-        <AccordionDetails>
-          <Typography>
-            {/* Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue. */}
+          <Typography variant="h6" sx={{ color: "text.secondary" }}>
+            {param4}
           </Typography>
-        </AccordionDetails>
+        </AccordionSummary>
+        <InputBox
+          parameter="Day-Of-Month"
+          value={param4}
+          setValue={setParam4}
+        />
+        <DoneButton setExpanded={setExpanded} panel={"panel4"} />
+        <AccordionDetails></AccordionDetails>
       </Accordion>
       <Accordion
         expanded={expanded === "panel5"}
@@ -110,18 +118,16 @@ export default function Panels() {
           aria-controls="panel5bh-content"
           id="panel5bh-header"
         >
-          <Typography variant="h6" sx={{ width: "33%", flexShrink: 0 }}>
+          <Typography variant="h6" sx={{ width: "50%", flexShrink: 0 }}>
             Month
           </Typography>
-        </AccordionSummary>
-        <InputBox parameter="Month" />
-        <DoneButton setExpanded={setExpanded} panel={"panel5"} />
-        <AccordionDetails>
-          <Typography>
-            {/* Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue. */}
+          <Typography variant="h6" sx={{ color: "text.secondary" }}>
+            {param5}
           </Typography>
-        </AccordionDetails>
+        </AccordionSummary>
+        <InputBox parameter="Month" value={param5} setValue={setParam5} />
+        <DoneButton setExpanded={setExpanded} panel={"panel5"} />
+        <AccordionDetails></AccordionDetails>
       </Accordion>
       <Accordion
         expanded={expanded === "panel6"}
@@ -132,18 +138,16 @@ export default function Panels() {
           aria-controls="panel6bh-content"
           id="panel6bh-header"
         >
-          <Typography variant="h6" sx={{ width: "33%", flexShrink: 0 }}>
+          <Typography variant="h6" sx={{ width: "50%", flexShrink: 0 }}>
             Day Of Week
           </Typography>
-        </AccordionSummary>
-        <InputBox parameter="Day-Of-Week" />
-        <DoneButton setExpanded={setExpanded} panel={"panel6"} />
-        <AccordionDetails>
-          <Typography>
-            {/* Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue. */}
+          <Typography variant="h6" sx={{ color: "text.secondary" }}>
+            {param6}
           </Typography>
-        </AccordionDetails>
+        </AccordionSummary>
+        <InputBox parameter="Day-Of-Week" value={param6} setValue={setParam6} />
+        <DoneButton setExpanded={setExpanded} panel={"panel6"} />
+        <AccordionDetails></AccordionDetails>
       </Accordion>
     </div>
   );
